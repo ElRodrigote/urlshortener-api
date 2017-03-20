5ab7f3398f0df626d2244e7eaf42a99ab51b026c
Rodrigo Ali <rodrigojali@gmail.com>
2017-03-20 16:08:34 +0000

    added up mongo db, connections and some test urls

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,41 @@
 var express = require('express');
-var router = express.Router();
 var mongodb = require('mongodb');
 var shortid = require('shortid');
 var validUrl = require('valid-url');
 
+var router = express.Router();
+var mLab = "mongodb://localhost:27017/url-shortener-microservice";
+var MongoClient = mongodb.MongoClient
+
 /* GET home page. */
 router.get('/new/:url(*)', function (req, res, next) {
-    res.send(req.params.url);
+    MongoClient.connect(mLab, function (err, db) {
+      if (err) {
+        console.log("Unable to connect to server", err);
+      } else {
+        console.log("Connected to server")
+      };
+      
+      var collection = db.collection('links');
+      var params = req.params.url;
+      
+      var newLink = function (db, callback) {
+        var insertLink = { url: params, short: "test" };
+        collection.insert([insertLink]);
+        res.send(params);
+      };
+ 
+      newLink(db, function () {
+        db.close();
+      });
+      
+    });
+    
+    
+
 });
+
+
+
 
 module.exports = router;
