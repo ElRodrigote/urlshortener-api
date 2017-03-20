1d52c1c3878472ac2d0dce89c131958474f04c0b
Rodrigo Ali <rodrigojali@gmail.com>
2017-03-20 16:20:35 +0000

    added up mongo db, connections and some test urls

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,48 @@
 var express = require('express');
-var mongodb = require('mongodb');
-var shortid = require('shortid');
-var validUrl = require('valid-url');
-
 var router = express.Router();
+ 
+var mongodb = require('mongodb');
 var mLab = "mongodb://localhost:27017/url-shortener-microservice";
 var MongoClient = mongodb.MongoClient
-
+ 
+var shortid = require('shortid');
+var validUrl = require('valid-url');
+ 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+ 
 router.get('/new/:url(*)', function (req, res, next) {
-    
-    res.send(req.params.url);
-    
-    MongoClient.connect(mLab, function (err, db) {
-      if (err) {
-        console.log("Unable to connect to server", err);
-      } else {
-        console.log("Connected to server")
-      };
-      
+  MongoClient.connect(mLab, function (err, db) {
+    if (err) {
+      console.log("Unable to connect to server", err);
+    } else {
+      console.log("Connected to server")
+     
       var collection = db.collection('links');
       var params = req.params.url;
-      
+     
       var newLink = function (db, callback) {
-        var insertLink = { url: params, short: "test" };
-        collection.insert([insertLink]);
-        res.send(params);
+        if (validUrl.isUri(params)) {
+          // if URL is valid, do this
+          var shortCode = shortid.generate();
+          var newUrl = { url: params, short: shortCode };
+          collection.insert([newUrl]);
+          res.json({ original_url: params, short_url: "localhost:3000/" + shortCode });
+        } else {
+         // if URL is invalid, do this
+          res.json({ error: "Wrong url format, make sure you have a valid protocol and real site." });
+        };
       };
- 
+     
       newLink(db, function () {
         db.close();
       });
-      
-    });
-    
-    
-
+     
+    };
+  });
+ 
 });
-
-
-
-
+ 
 module.exports = router;
