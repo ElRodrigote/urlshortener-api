8c7c77398b1d1eb71374e6fe4d425b9657c82b80
Rodrigo Ali <rodrigojali@gmail.com>
2017-03-20 15:32:51 +0000

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var mongodb = require('mongodb');
+var shortid = require('shortid');
+var validUrl = require('valid-url');
+
+/* GET home page. */
+router.get('/new/:url(*)', function (req, res, next) {
+    res.send(req.params.url);
+});
+
+module.exports = router;
